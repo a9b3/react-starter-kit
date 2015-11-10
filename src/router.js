31f9122ece7544fb6fc02523300072023ab998dd
Sam Lau <esayemm@gmail.com>
2015-11-10 01:08:28 -0800

    router

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
-import App from './app/App.js';
+import App from './app/App.react.js';
 
-const routes = {
-    path: '/',
-    component: App,
-    // childRoutes: [
-    //     { path: 'about', component: Test },
-    // ]
-};
+const routes = [
+    {
+        path: '/',
+        component: App,
+        // childRoutes: [
+        //     { path: 'about', component: Test },
+        // ]
+    },
+];
 
 module.exports = routes;
