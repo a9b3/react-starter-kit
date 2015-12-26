ad453928b3859755d7339937389a1b969890bca3
Sam Lau <esayemm@gmail.com>
2015-12-26 01:45:38 -0800

    use jsx syntax for router

--- src/router.js
+++ src/router.js
@@ -4,36 +4,17 @@ import { createHistory } from 'history';
 import App from './app/App.react.js';
 import Default from './app/Default.react.js';
 import Flex from './app/Flex.react.js';
-
-const routes = [
-    {
-        path: '/',
-        component: App,
-        childRoutes: [
-            {
-                path: '/main',
-                name: 'main',
-                component: Default,
-            },
-            {
-                path: '/flex',
-                name: 'flex',
-                component: Flex,
-            },
-            {
-                path: '/test',
-                name: 'test',
-                component: require('./app/Test.react.js'),
-            },
-        ],
-    },
-];
+import Test from './app/Test.react.js';
 
 const history = createHistory();
 
 const router = (
-    <Router history={history}
-        routes={routes}>
+    <Router history={history}>
+        <Route path="/" component={App}>
+            <Route path="main" component={Default} />
+            <Route path="flex" component={Flex} />
+            <Route path="test" component={Test} />
+        </Route>
     </Router>
 );
 
