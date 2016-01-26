9e2b94f9130aee0ae48b16b56f398d560fba74ce
Sam Lau <esayemm@gmail.com>
2016-01-25 23:01:42 -0800

    example service state

---
+++ src/services/example-state.js
@@ -0,0 +1,46 @@
+'use strict';
+
+export const actions = {
+  add(text) {
+    exampleState.add(text);
+    return {
+      type: 'UPDATE_EXAMPLE',
+    };
+  },
+
+  remove(i) {
+    exampleState.remove(i);
+    return {
+      type: 'UPDATE_EXAMPLE',
+    };
+  }
+};
+
+export function reducer(state = exampleState, action) {
+  switch(action.type) {
+  case 'UPDATE_EXAMPLE':
+    return Object.assign({}, state, exampleState);
+  default:
+    return state;
+  }
+};
+
+class ExampleState {
+  constructor() {
+    this.todos = [
+      'Clean room',
+      'Learn React',
+    ];
+  }
+
+  add(text) {
+    this.todos.push(text);
+  }
+
+  remove(i) {
+    this.todos.splice(i, 1);
+  }
+};
+
+const exampleState = new ExampleState();
+export default exampleState;
