caa10486edde74a24d64f1eaaa0da94712c6bc9d
Sam Lau <esayemm@gmail.com>
2016-01-25 23:02:31 -0800

    examplea ctions

--- src/actions.js
+++ src/actions.js
@@ -1,62 +1,25 @@
-// EXAMPLE:
-// keys
-//
-// export const SELECT_REDDIT = 'SELECT_REDDIT';
-// export const INVALIDATE_REDDIT = 'INVALIDATE_REDDIT';
-// export const REQUEST_POSTS = 'REQUEST_POSTS';
-// export const RECEIVE_POSTS = 'RECEIVE_POSTS';
-
-// EXAMPLE:
-// sync action
-//
-// export function selectReddit(reddit) {
-//     return {
-//         type: SELECT_REDDIT,
-//         reddit,
-//     };
-// }
-
-// EXAMPLE:
-// async action
-//
-// export function fetchPosts(reddit) {
-//     return function(dispatch) {
-//         dispatch(requestPosts(reddit));
-//
-//         return axios.get(`http://www.reddit.com/r/${reddit}.json`)
-//             .then(response => response.data)
-//             .then(json => {
-//                 dispatch(receivePosts(reddit, json));
-//             });
-//     };
-// }
-//
-// function shouldFetchPosts(state, reddit) {
-//     const posts = state.postsByReddit[reddit];
-//     if (!posts) {
-//         return true;
-//     } else if (posts.isFetching) {
-//         return false;
-//     } else {
-//         return posts.didInvalidate;
-//     }
-// }
-//
-// export function fetchPostsIfNedded(reddit) {
-//     return (dispatch, getState) => {
-//         if (shouldFetchPosts(getState(), reddit)) {
-//             return dispatch(fetchPosts(reddit));
-//         } else {
-//             return Promise.resolve();
-//         }
-//     };
-// }
-
+/*
+ * Entry point for all exported actions.
+ * Import actions and export them here. This will simplify the dependencies from
+ * components that require them. React components can simply do the following:
+ *
+ *    import { foo } from 'root/actions.js';
+ *
+ * Seperate logic between services and react/redux components. Services that
+ * want to communicate will have to expose actions and reducers. This file will
+ * handle the actions.
+ */
 'use strict';
 
 import axios from 'axios';
 import { pushPath } from 'redux-simple-router';
 
-export default {
+/*
+ * import actions exported by services here
+ * ex:
+ * import { actions as foo } from 'services/foo.js';
+ * exports['foo'] = foo;
+ */
 
-};
+import { actions as exampleStateActions } from 'services/example-state.js';
+exports['exampleState'] = exampleStateActions;
