8fe5770cef4fcaad2a89e5799d284c71f5f94a76
Sam Lau <esayemm@gmail.com>
2016-01-31 04:49:45 -0800

    debug /style_guide page

---
+++ src/containers/style-guide/style-guide.container.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import './style-guide.scss';
+import React from 'react';
+
+class StyleGuideContainer extends React.Component {
+  render() {
+    return (
+      <div className="style-guide">
+        hi
+      </div>
+    );
+  }
+}
+
+export default StyleGuideContainer;
