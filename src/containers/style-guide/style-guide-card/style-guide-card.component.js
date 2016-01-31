9c7a03c862a7ca69999a40785a66095705afdb8e
Sam Lau <esayemm@gmail.com>
2016-01-31 05:31:30 -0800

    style_guide page

---
+++ src/containers/style-guide/style-guide-card/style-guide-card.component.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import './style-guide-card.scss';
+import React, { PropTypes } from 'react';
+
+const StyleGuideCardComponent = props =>
+<div className="style-guide__card">
+  <div className="style-guide__card__header">
+    <h2>
+    {props.title}
+    </h2>
+  </div>
+  <div className="style-guide__card__content">
+    {props.children}
+  </div>
+</div>;
+
+StyleGuideCardComponent.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.element,
+};
+export default StyleGuideCardComponent;
--- src/containers/style-guide/style-guide.container.js
+++ src/containers/style-guide/style-guide.container.js
@@ -3,11 +3,65 @@
 import './style-guide.scss';
 import React from 'react';
 
+import StyleGuideCardComponent from './style-guide-card/style-guide-card.component.js';
+
 class StyleGuideContainer extends React.Component {
   render() {
     return (
       <div className="style-guide">
-        hi
+        <StyleGuideCardComponent title={'typography'}>
+          <div>
+            <h1>Header 1</h1>
+          </div>
+          <div>
+            <h2>Header 2</h2>
+          </div>
+          <div>
+            <h3>Header 3</h3>
+          </div>
+          <div>
+            <h4>Header 4</h4>
+          </div>
+          <div>
+            <h5>Header 5</h5>
+          </div>
+          <div>
+            <h6>Header 6</h6>
+          </div>
+          <hr/>
+          <div>
+            <p>
+              Paragraph<br/><br/>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend
+              velit id turpis fringilla volutpat. Nulla tristique sem ex, ac dignissim odio
+              volutpat sit amet.
+            </p>
+          </div>
+          <div>
+            Normal text<br/><br/>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend
+            velit id turpis fringilla volutpat. Nulla tristique sem ex, ac dignissim odio
+            volutpat sit amet.
+          </div>
+          <div>
+            <b>b tag</b>
+          </div>
+          <div>
+            <a href="#">a tag</a>
+          </div>
+        </StyleGuideCardComponent>
+
+        <StyleGuideCardComponent title={'components'}>
+          <div>
+            <button>Button</button>
+          </div>
+          <hr/>
+          <div>
+            <form>
+              <input type="text" placeholder="input"/>
+            </form>
+          </div>
+        </StyleGuideCardComponent>
       </div>
     );
   }
