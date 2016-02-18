00d4e9cf4a889d2ccefd73f3709cdc44a2e20043
Sam Lau <esayemm@gmail.com>
2016-02-18 15:31:49 -0800

    does not have these reducers

--- src/root-reducer.js
+++ src/root-reducer.js
@@ -1,11 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
-import { reducer as mixer } from './services/mixer.js';
-import { reducer as transport } from './services/transport.js';
 
 const rootReducer = combineReducers({
-  mixer,
-  transport,
   routing,
 });
 
