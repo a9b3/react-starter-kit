6f41036f936978da39f5f9140b7515db8935ee98
Sam Lau <esayemm@gmail.com>
2016-02-18 15:37:37 -0800

    reorg imports

--- src/index.js
+++ src/index.js
@@ -4,25 +4,22 @@ import '!style-loader!css-loader!normalize.css';
 import 'font-awesome/scss/font-awesome.scss';
 import '!style-loader!css-loader!animate.css/animate.css';
 import '!style-loader!css-loader!highlight.js/styles/github.css';
-// custom
 import 'root/styles/index.scss';
 
 // Global polyfill Promise
 global.Promise = require('rsvp').Promise;
 
+import React from 'react';
+import { browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux';
+import { render } from 'react-dom';
 import config from 'config';
+import createStore from './store.js';
+import Root from './containers/root.js';
 
 if (!config.DEBUG && navigator.serviceWorker) {
   navigator.serviceWorker.register('/service-worker.js');
 }
-
-// deps
-import createStore from './store.js';
-import { browserHistory } from 'react-redux';
-import { syncHistoryWithStore } from 'react-router-redux';
-import { render } from 'react-dom';
-import React from 'react';
-import Root from './containers/root.js';
 
 import './initialize.js';
 
