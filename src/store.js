25d1d60cd6be6e8d67ff64ffa89071d9f3a8b6aa
Sam Lau <esayemm@gmail.com>
2016-03-12 02:37:04 -0800

    destroy these semis

--- src/store.js
+++ src/store.js
@@ -1,15 +1,15 @@
-import config from 'config';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
-import rootReducer from './root-reducer.js';
+import config from 'config'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+import rootReducer from './root-reducer.js'
 
 export default function (initialState) {
   const middlewares = [
     thunkMiddleware,
     config.DEBUG && createLogger(),
   ]
-  .filter(a => a);
+  .filter(a => a)
 
   return createStore(
     rootReducer,
@@ -17,5 +17,5 @@ export default function (initialState) {
     compose(
       applyMiddleware(...middlewares)
     )
-  );
+  )
 }
