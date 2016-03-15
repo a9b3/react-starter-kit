f587fbeb79876222084a7366b19358dbc1f1c816
Sam Lau <esayemm@gmail.com>
2016-03-15 15:44:19 -0700

    using html webpack injection change server

--- gulpfile.js
+++ gulpfile.js
@@ -91,8 +91,8 @@ gulp.task('server:dev', done => {
   const express = require('express');
   const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpackHotMiddleware = require('webpack-hot-middleware');
-  const wpConfig = require('./webpack.config.dev.js');
 
+  const wpConfig = require('./webpack.config.dev.js');
   const compiler = webpack(wpConfig);
 
   const app = express();
@@ -100,17 +100,23 @@ gulp.task('server:dev', done => {
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: wpConfig.output.publicPath,
-  }));
+  }))
 
   app.use(webpackHotMiddleware(compiler));
 
-  // this is for static assets
+  // static assets
   app.use(express.static('src'));
 
-  // route every request to index.html, let client handle page routing
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-  });
+  // let client handle page routing
+  app.get('*', (req, res, next) => {
+    // using html webpack injection, serve the index.html file in memory
+    const filename = path.join(compiler.outputPath, 'index.html')
+    compiler.outputFileSystem.readFile(filename, (e, content) => {
+      if (e) return next(e)
+      res.set('content-type', 'text/html')
+      res.send(content)
+    })
+  })
 
   app.listen(config.port, 'localhost', e => {
     if (e) return console.log(e);
