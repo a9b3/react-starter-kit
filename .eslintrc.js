fc9ebac7bfef3b87db680d0b272e13e43b800e93
Sam Lau <esayemm@gmail.com>
2016-03-16 01:18:13 -0700

    use .eslintrc.js not json

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "react"
+  ],
+  "parser": "babel-eslint",
+  "ecmaFeatures": {
+    "spread": true,
+    "restParams": true,
+    "experimentalObjectRestSpread": true,
+  },
+  "rules": {
+    "semi": [2, "never"],
+    "quotes": [0],
+    "no-unused-vars": [2, {
+      "argsIgnorePattern": "^_",
+    }],
+    "consistent-return": [0],
+    "no-param-reassign": [0],
+    "no-undef": [0],
+    "arrow-body-style": [0],
+  },
+};
--- gulp/lint.js
+++ gulp/lint.js
@@ -6,26 +6,26 @@ const gulp = require('gulp');
 module.exports = function(config) {
   gulp.task('lint:js', () => {
     return gulp.src([
-      './gulpfile.js',
-      config.src + '/**/*.js',
-      '!' + 'node_modules/**/*',
-      '!' + 'dist/**/*',
+      `gulpfile.js`,
+      `${config.src}/**/*.js`,
+      `!node_modules/**/*`,
+      `!dist/**/*`,
     ])
     .pipe($.gitmodified(['added']))
-    .pipe($.eslint())
+    .pipe($.eslint(require('../.eslintrc.js')))
     .pipe($.eslint.format())
     .pipe($.eslint.failAfterError());
   });
 
   gulp.task('lint:scss', () => {
     return gulp.src([
-      config.src + '/**/*.scss',
-      '!' + 'node_modules/**/*',
-      '!' + 'dist/**/*',
+      `${config.src}/**/*.scss`,
+      `!node_modules/**/*`,
+      `!dist/**/*`,
     ])
     .pipe($.gitmodified(['added']))
     .pipe($.scssLint({
-      config: '.scss-lint.yml',
+      config: '../scss-lint.yml',
     }))
     .pipe($.scssLint.failReporter('E'))
   });
