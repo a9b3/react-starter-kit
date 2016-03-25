41a33cffa7111f66e81eb46475193c0df444c535
Sam Lau <esayemm@gmail.com>
2016-03-25 14:20:08 -0700

    import css version of fa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // app entry point
 // styles
 import '!style-loader!css-loader!normalize.css'
-import 'font-awesome/scss/font-awesome.scss'
+import '!style-loader!css-loader!font-awesome/css/font-awesome.css'
 import '!style-loader!css-loader!animate.css/animate.css'
 import '!style-loader!css-loader!highlight.js/styles/github.css'
 import 'root/styles/index.scss'
