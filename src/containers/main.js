8edf7ca11901726e86fca2bd77051707a747632b
Sam Lau <esayemm@gmail.com>
2016-03-25 14:24:50 -0700

    (init): use main container for init logic

---
+++ src/containers/main.js
@@ -0,0 +1,48 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+/*
+ * Top level container
+ * Purpose is to house init logic and top level components (eg. toasts)
+ */
+class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+      ready: false,
+    }
+    this._init = this._init.bind(this)
+  }
+
+  componentWillMount() {
+    this._init()
+  }
+
+  _init() {
+    // init logic here
+    // call this.setState once everything is done
+    setTimeout(() => {
+      this.setState({
+        ready: true,
+      })
+    }, 500)
+  }
+
+  render() {
+    const {
+      ready,
+    } = this.state
+
+    if (!ready) {
+      return <div className="loading">
+        <i className="fa fa-spinner fa-spin"></i>
+      </div>
+    } else {
+      return <div>
+        {this.props.children}
+      </div>
+    }
+  }
+}
+
+export default Main
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { Route } from 'react-router'
+
+/*
+ * Components
+ */
+import Main from './containers/main.js'
 import NotFoundContainer from './containers/not-found/not-found.container.js'
 
 export default (
-  <Route path="/">
+  <Route
+    path="/"
+    component={Main}
+  >
     <Route path="*" component={NotFoundContainer} />
   </Route>
 )
