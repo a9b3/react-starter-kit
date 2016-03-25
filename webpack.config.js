0cf774a31a980e2fbe7f2ceabf53108b2e11dc8d
Sam Lau <esayemm@gmail.com>
2016-03-25 03:05:02 -0700

    use extract text in production

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@ const path = require('path')
 const webpack = require('webpack')
 const webpackPlugins = require('webpack-load-plugins')()
 
+const bundleNames = {
+  js: 'bundle.[hash:5].js',
+  css: 'bundle.[hash:5].css',
+}
+
 const configs = {}
 
 const sharedPlugins = [
@@ -13,6 +18,46 @@ const sharedPlugins = [
   }),
 ]
 
+const sharedLoaders = [
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    loader: 'babel',
+    query: {
+      cacheDirectory: true,
+    },
+  },
+  {
+    test: /\.html$/,
+    loaders: ['html'],
+  },
+  {
+    test: /\.(png|jpg|jpeg)$/,
+    // inline ULRS for <= 8k images, direct URLs else
+    loaders: ['url-loader?limit=8192'],
+  },
+  {
+    test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+    loader: "url?limit=10000&mimetype=application/font-woff",
+  },
+  {
+    test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+    loader: "url?limit=10000&mimetype=application/font-woff",
+  },
+  {
+    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+    loader: "url?limit=10000&mimetype=application/octet-stream",
+  },
+  {
+    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+    loader: "file",
+  },
+  {
+    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+    loader: "url?limit=10000&mimetype=image/svg+xml",
+  },
+]
+
 configs.prod = {
   entry: [
     './src/index.js',
@@ -20,11 +65,12 @@ configs.prod = {
 
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: bundleNames.js,
     publicPath: '/',
   },
 
   plugins: sharedPlugins.concat([
+    new webpackPlugins.extractText(bundleNames.css),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
@@ -53,62 +99,22 @@ configs.prod = {
   },
 
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          cacheDirectory: true,
-        },
-      },
+    loaders: sharedLoaders.concat([
       {
         test: /\.scss$/,
-        loaders: [
-          'style?sourceMap',
-          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]&sourceMap',
-          'postcss',
-          'sass?sourceMap',
-        ],
+        loader: webpackPlugins.extractText.extract(
+          'style-loader',
+          'css?modules&importLoaders=2&localIdentName=[path]___[name]__[local]___[hash:base64:5]!postcss-loader!sass'
+        ),
       },
       {
         test: /\.css$/,
-        loaders: [
+        loader: webpackPlugins.extractText.extract(
           'style',
-          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]$sourceMap',
-          'postcss',
-        ],
+          'css?modules&importLoaders=2&localIdentName=[path]___[name]__[local]___[hash:base64:5]!postcss-loader!sass'
+        ),
       },
-      {
-        test: /\.html$/,
-        loaders: ['html'],
-      },
-      {
-        test: /\.(png|jpg|jpeg)$/,
-        // inline ULRS for <= 8k images, direct URLs else
-        loaders: ['url-loader?limit=8192'],
-      },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/font-woff",
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/font-woff",
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/octet-stream",
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file",
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=image/svg+xml",
-      },
-    ],
+    ]),
   },
 
   postcss() {
@@ -131,6 +137,28 @@ configs.dev = Object.assign({}, configs.prod, {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
   ]),
+
+  module: {
+    loaders: sharedLoaders.concat([
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style?sourceMap',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]&sourceMap',
+          'postcss',
+          'sass?sourceMap',
+        ],
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]$sourceMap',
+          'postcss',
+        ],
+      },
+    ]),
+  },
 })
 
 module.exports = (process.env.NODE_ENV === 'production') ? configs.prod : configs.dev
