cfe27076173bafee9f62738ed5dc54a1ee8714ad
Sam Lau <esayemm@gmail.com>
2016-05-04 18:45:03 -0700

    remove eslint flow and otherunused packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,3 @@
 module.exports = {
-  "extends": "airbnb",
-  "plugins": [
-    "react"
-  ],
-  "parser": "babel-eslint",
-  "ecmaFeatures": {
-    "spread": true,
-    "restParams": true,
-    "experimentalObjectRestSpread": true,
-  },
-  "rules": {
-    "semi": [2, "never"],
-    "quotes": [0],
-    "no-unused-vars": [2, {
-      "argsIgnorePattern": "^_",
-    }],
-    "consistent-return": [0],
-    "no-param-reassign": [0],
-    "no-undef": [0],
-    "arrow-body-style": [0],
-    "new-cap": [0],
-    "react/wrap-multilines": [0],
-  },
+
 };
