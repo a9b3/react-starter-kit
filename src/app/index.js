d86d8555e46a124950c8615f70d376b5c25dec9e
Sam Lau <esayemm@gmail.com>
2016-05-04 19:32:33 -0700

    remove animate.css

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,5 @@
 // app entry point
 import '!style-loader!css-loader!font-awesome/css/font-awesome.css'
-import '!style-loader!css-loader!animate.css/animate.css'
 import '!style-loader!css-loader!highlight.js/styles/github.css'
 import 'styles/index.scss'
 
