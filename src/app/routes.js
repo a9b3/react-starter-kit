1f20efe7f900f7cec8431909e597af000275ab15
Sam Lau <esayemm@gmail.com>
2016-07-24 05:41:10 -0700

    fix up some default cmoponents

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Route } from 'react-router'
-import Main from './components/main.js'
+import { Route, IndexRedirect } from 'react-router'
 import NotFoundContainer from './components/not-found/not-found.js'
+import HelloWorld from './components/hello-world/hello-world.js'
 
 export default (
-  <Route
-    path='/'
-    component={Main}
-  >
+  <Route path='/'>
+    <IndexRedirect to='hello' />
+    <Route path='hello' component={HelloWorld} />
     <Route path='*' component={NotFoundContainer} />
   </Route>
 )
