d52c4e10dd5ce3e6ef2c94bf15ef53f4322e069f
Sam Lau <esayemm@gmail.com>
2016-07-25 20:46:40 -0700

    reset to just hello world

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,22 +1,13 @@
 import React from 'react'
 import { Route, IndexRedirect } from 'react-router'
+import HelloWorld from './containers/hello-world/hello-world.js'
 import NotFoundContainer from './containers/not-found/not-found.js'
-
-import StyleguideShell from './containers/styleguide/shell.js'
-import General from './containers/styleguide/general/general.js'
-import Layout from './containers/styleguide/layout/layout.js'
 
 export default (
   <Route path='/'>
 
-    <IndexRedirect to='styleguide' />
-
-    <Route path='styleguide' component={StyleguideShell}>
-      <IndexRedirect to='general' />
-      <Route path='general' component={General} />
-      <Route path='layout' component={Layout} />
-    </Route>
-
+    <IndexRedirect to='hello-world' />
+    <Route path='hello-world' component={HelloWorld} />
     <Route path='*' component={NotFoundContainer} />
   </Route>
 )
