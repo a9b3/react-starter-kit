3c27e7beff06ac1c4c75e67edb496e4604bac2b3
Sam Lau <esayemm@gmail.com>
2016-08-05 01:52:21 -0700

    tests

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,11 +1,70 @@
-import React from 'react'
-import should from 'should'
-import { shallow } from 'enzyme'
+import React, { Component } from 'react'
+import expect from 'expect'
+import sinon from 'sinon'
+import { shallow, mount } from 'enzyme'
+
+function nextTick(fn) {
+  return new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      try {
+        await fn()
+        resolve()
+      } catch (e) {
+        reject(e)
+      }
+    }, 1)
+  })
+}
 
 describe('write tests', () => {
+  const foohelper = {
+    fooAsync() {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve('foo')
+        }, 1000)
+      })
+    },
+  }
+  class Foo extends Component {
+    state = {}
+
+    async componentDidMount() {
+      const res = await foohelper.fooAsync()
+      this.setState({ res })
+    }
+
+    render() {
+      return <div id='one'>
+        <div id='two'>
+        </div>
+        {
+          this.state.res && <div id='res'>
+            {this.state.res}
+          </div>
+        }
+      </div>
+    }
+  }
+
   it('render dom without a problems', async () => {
     const root = shallow(<div />)
-    should.exist(root)
-    root.find('div').length.should.equal(1)
+    expect(root.find('div').length).toBe(1)
+  })
+
+  it('test components with enzyme', async () => {
+    const wrapper = mount(<Foo />)
+    expect(wrapper.find('#one').length).toBe(1)
+    expect(wrapper.find('#two').length).toBe(1)
+  })
+
+  it('stub out async functions', async () => {
+    const fooAsyncStub = sinon.stub(foohelper, 'fooAsync').returns(Promise.resolve('hi'))
+    const wrapper = mount(<Foo />)
+
+    await nextTick(() => {
+      sinon.assert.calledOnce(fooAsyncStub)
+      expect(wrapper.state().res).toBe('hi')
+    })
   })
 })
